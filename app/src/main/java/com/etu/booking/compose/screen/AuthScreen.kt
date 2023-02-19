ec6b29b8e0bba53465aa77b0a1b6e414bbc62aca
alelambin <lambin1483alex@gmail.com>
2023-02-19 22:43:23 +0300

    [auth-screen] Add auth screen

---
+++ app/src/main/java/com/etu/booking/compose/screen/AuthScreen.kt
@@ -0,0 +1,201 @@
+package com.etu.booking.compose.screen
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.etu.booking.view.AuthViewModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun AuthScreen(viewModel: AuthViewModel = viewModel()) {
+    var state by remember { mutableStateOf(0) }
+    val titles = listOf("Sign In", "Sign Up")
+
+    Column {
+        TabRow(selectedTabIndex = state) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    text = {
+                        Text(text = title)
+                    },
+                    selected = state == index,
+                    onClick = {
+                        state = index
+                    }
+                )
+            }
+        }
+        if (state == 0) {
+            SignIn(viewModel)
+        } else {
+            SignUp(viewModel)
+        }
+    }
+}
+
+@Composable
+fun SignIn(
+    viewModel: AuthViewModel
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(
+                horizontal = 8.dp,
+                vertical = 16.dp
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AuthInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.login,
+            placeholder = "Login",
+            onValueChange = {
+                viewModel.login = it
+            },
+        )
+        AuthInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.password,
+            placeholder = "Password",
+            onValueChange = {
+                viewModel.password = it
+            },
+            visualTransformation = PasswordVisualTransformation(),
+        )
+        AuthButton(text = "Sign In") {
+            // TODO: sign in
+        }
+    }
+}
+
+@Composable
+fun SignUp(
+    viewModel: AuthViewModel
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(
+                horizontal = 8.dp,
+                vertical = 16.dp
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val context = LocalContext.current
+        val dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        AuthInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.name,
+            placeholder = "Name",
+            onValueChange = {
+                viewModel.name = it
+            },
+        )
+        AuthInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.surname,
+            placeholder = "Surname",
+            onValueChange = {
+                viewModel.surname = it
+            },
+        )
+        AuthInput(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    showDatePickerDialog(context, viewModel.birthdate) {
+                        date -> viewModel.birthdate = date
+                    }
+                },
+            value = dateFormat.format(viewModel.birthdate),
+            placeholder = "Birthdate",
+            onValueChange = {
+                viewModel.birthdate = LocalDate.parse(it, dateFormat)
+            },
+            isEnabled = false,
+        )
+        AuthInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.login,
+            placeholder = "Login",
+            onValueChange = {
+                viewModel.login = it
+            },
+        )
+        AuthInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.password,
+            placeholder = "Password",
+            onValueChange = {
+                viewModel.password = it
+            },
+            visualTransformation = PasswordVisualTransformation(),
+        )
+        AuthButton(text = "Add document photo") {
+            // TODO: get access to camera or gallery
+        }
+        AuthButton(text = "Sign Up") {
+            // TODO: sign up
+        }
+    }
+}
+
+@Composable
+fun AuthInput(
+    modifier: Modifier,
+    value: String,
+    placeholder: String,
+    onValueChange: (String) -> Unit,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    isEnabled: Boolean = true
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        visualTransformation = visualTransformation,
+        placeholder = {
+            Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.LightGray))
+        },
+        enabled = isEnabled,
+    )
+}
+
+@Composable
+fun AuthButton(
+    text: String, 
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = onClick,
+        border = BorderStroke(1.dp, Color.Black),
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = text)
+        }
+    }
+}
---
+++ app/src/main/java/com/etu/booking/view/AuthViewModel.kt
@@ -0,0 +1,33 @@
+package com.etu.booking.view
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.etu.booking.model.AuthModel
+import com.etu.booking.model.PersonModel
+import java.time.LocalDate
+
+class AuthViewModel : ViewModel()  {
+
+    private var authModel = AuthModel(
+        login = "",
+        password = "",
+        personModel = PersonModel(
+            login = "",
+            password = "",
+            name = "",
+            surname = "",
+            birthdate = LocalDate.now(),
+            avatarResource = null
+        )
+    )
+
+    var login by mutableStateOf(authModel.login)
+    var password by mutableStateOf(authModel.password)
+    var name by mutableStateOf(authModel.personModel.name)
+    var surname by mutableStateOf(authModel.personModel.surname)
+    var birthdate by mutableStateOf(authModel.personModel.birthdate)
+    var avatarResource by mutableStateOf(authModel.personModel.avatarResource)
+
+}
