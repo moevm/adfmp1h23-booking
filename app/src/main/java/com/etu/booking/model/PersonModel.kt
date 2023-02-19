3d03da69cde030ee10a34092838a6c2516d319b9
alelambin <lambin1483alex@gmail.com>
2023-02-19 15:50:30 +0300

    [person-screen] Add person screen

---
+++ app/src/main/java/com/etu/booking/model/PersonModel.kt
@@ -0,0 +1,19 @@
+package com.etu.booking.model
+
+import androidx.annotation.DrawableRes
+import java.time.LocalDate
+import javax.validation.constraints.Past
+
+data class PersonModel(
+
+    var name: String,
+
+    var surname: String,
+
+    @field:Past
+    var birthdate: LocalDate,
+
+    @DrawableRes
+    val avatarResource: Int? = null
+
+)
