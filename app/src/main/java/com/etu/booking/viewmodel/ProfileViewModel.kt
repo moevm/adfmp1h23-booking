ce05c3013a49158c828a483d44a78427b288998b
Vladimir Arutiunian <vladimir.arutiunian.var@gmail.com>
2023-02-28 23:50:32 +0300

    [saving-sign-up-data] Add the saving registration data. Change a callback after loading a image to a previous screen

--- app/src/main/java/com/etu/booking/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/etu/booking/viewmodel/ProfileViewModel.kt
@@ -1,6 +1,8 @@
 package com.etu.booking.viewmodel
 
+import com.etu.booking.default.DefaultModels
 import com.etu.booking.default.DefaultModels.PERSON_MODEL
+import com.etu.booking.model.AuthModel
 import com.etu.booking.model.PersonModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,11 +11,38 @@ import kotlinx.coroutines.flow.update
 
 class ProfileViewModel : ViewModelWithLoading() {
 
+    private val personsDatabase = mutableListOf(PERSON_MODEL)
+
     private val _person = MutableStateFlow<PersonModel?>(null).apply { drop(1) }
 
     val person = _person.asStateFlow()
 
     fun updateProfile() = launchWithLoading {
-        _person.update { PERSON_MODEL }
+        _person.update { personsDatabase.first() }
+    }
+
+    fun updateProfileFromAuthState(
+        authModel: AuthModel,
+    ) = launchWithLoading { // TODO: temporary, it will be removed
+        val personModel = authModel.personModel
+        val passportModel = authModel.personModel.passport
+        val newPerson = PersonModel(
+            login = authModel.login,
+            password = authModel.password,
+            name = authModel.personModel.name,
+            surname = personModel.surname,
+            birthdate = personModel.birthdate,
+            avatarResource = personModel.avatarResource ?: DefaultModels.DEFAULT_AVATAR,
+            passport = PersonModel.PassportModel(
+                nationality = passportModel.nationality,
+                number = passportModel.number,
+                expiresAt = passportModel.expiresAt,
+            ),
+        )
+
+        _person.update {
+            personsDatabase[0] = newPerson
+            newPerson
+        }
     }
 }
